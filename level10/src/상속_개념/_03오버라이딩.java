f051150692fdd22998667e334700a2b807203f50
yeonmi park <pyeonm25@gmail.com>
2025-01-16 12:19:02 +0900

    create 접근제어자 파이널

--- level10/src/상속_개념/_03오버라이딩.java
+++ level10/src/상속_개념/_03오버라이딩.java
@@ -1,7 +1,8 @@
 package 상속_개념;
 
+// super class
+// 부모클래스 , 슈퍼 클래스 , 상위 클래스 
 class Parent{
-	
 	int num = 100;
 	void test1() {
 		System.out.println("test1 호출!!!!");
@@ -10,6 +11,9 @@ class Parent{
 		System.out.println("num=" + num);
 	}
 }
+
+// sub class
+// 자식클래스, 서브클래스 , 하위클래스 
 class Child extends Parent{
 	int num = 200; 
 	void test2() {
