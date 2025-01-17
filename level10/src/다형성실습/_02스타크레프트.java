4c6e3ab44f3b50288fe7726562dce1b10b951414
yeonmi park <pyeonm25@gmail.com>
2025-01-17 10:52:02 +0900

    Create _02스타크레프트.java

---
+++ level10/src/다형성실습/_02스타크레프트.java
@@ -0,0 +1,22 @@
+package 다형성실습;
+
+/*
+ * 
+ // damage() 메소드를 가진 
+// Interface Damageable 를 정의하고
+// 최하위 유닛들 (모든) 에게 Damageable 상속 
+// main에서 각각의 유닛들을 손상시킨 후 -> repair() 돌려보기
+ * 
+ * 
+ */
+
+public class _02스타크레프트 {
+	public static void main(String[] args) {
+		
+		// 10개 유닛 만들어서 
+		
+		// 서로 랜덤하게 공격하다가 
+		// 한명 살아남을때까지 반복! 
+		
+	}
+}
