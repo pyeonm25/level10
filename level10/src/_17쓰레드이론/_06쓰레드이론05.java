c08bb9a7dec63ae9d235c191c162418d3b6befd2
yeonmi park <pyeonm25@gmail.com>
2025-01-23 14:56:15 +0900

    .
    .

--- level10/src/_17쓰레드이론/_06쓰레드이론05.java
+++ level10/src/_17쓰레드이론/_06쓰레드이론05.java
@@ -2,8 +2,8 @@ package _17쓰레드이론;
 
 import java.util.Random;
 
-class CustomerRun implements Runnable{
-	
+class CustomerRun implements Runnable {
+
 	String name;
 	ATM atmToUse;
 	int needMoney;
@@ -14,12 +14,11 @@ class CustomerRun implements Runnable{
 		this.needMoney = needMoney;
 	}
 
-
 	@Override
 	public void run() {
-		while(atmToUse.getBalance() >= needMoney) {
+		while (atmToUse.getBalance() >= needMoney) {
 			atmToUse.withdraw(name, needMoney);
-			
+
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
@@ -27,36 +26,56 @@ class CustomerRun implements Runnable{
 			}
 		}
 	}
-	
+
 }
 
-class ATM{
-	private int balance =0;
+class ATM {
+	private int balance = 0;
+
 	public void addMoney(int amount) {
 		balance += amount;
 	}
+
 	public int getBalance() {
 		return balance;
 	}
-	
+	// 동기화 메서드 : 동시에 여러 쓰레드가 사용 불가능
+	// 한번에 한 쓰레드만 이용할 수 있다 .
+
+	// public synchronized void withdraw(String name, int amount) {
 	public void withdraw(String name, int amount) {
-		
-		if(balance < amount) return;
-		
-		System.out.printf(" %s 인출 요청 (현재 잔액 %d) \n" , name, balance);
-		try {
-			Thread.sleep(new Random().nextInt(301)+700);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+
+		// 동기화 블럭 this => 현재 쓰레드를 의미한다 
+		synchronized (this) {
+			if (balance < amount)
+				return;
+
+			System.out.printf(" %s 인출 요청 (현재 잔액 %d) \n", name, balance);
+			try {
+				Thread.sleep(new Random().nextInt(301) + 700);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			balance -= amount;
+
+			System.out.printf(" %s 인출 완료 (현재 잔액 %d) \n", name, balance);
+			System.out.println("----------------");
 		}
-		balance -= amount;
-		
-		System.out.printf(" %s 인출 완료 (현재 잔액 %d) \n" , name, balance);
 	}
 }
 
 public class _06쓰레드이론05 {
 	public static void main(String[] args) {
-		
+		ATM atm = new ATM();
+
+		atm.addMoney(5000);
+
+		Thread thr1 = new Thread(new CustomerRun("철수", atm, 500));
+		Thread thr2 = new Thread(new CustomerRun("영희", atm, 300));
+		Thread thr3 = new Thread(new CustomerRun("개똥이", atm, 400));
+
+		thr1.start();
+		thr2.start();
+		thr3.start();
 	}
 }
